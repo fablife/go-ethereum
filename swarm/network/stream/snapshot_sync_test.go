22e01c2af500259af9c6be59d109535fbff276d3
Fabio Barone <holistic.computing@gmail.com>
2018-08-02 13:31:25 -0500

    swarm/network/stream: temp remove warn log

--- swarm/network/stream/snapshot_sync_test.go
+++ swarm/network/stream/snapshot_sync_test.go
@@ -244,7 +244,7 @@ func testSyncingViaGlobalSync(t *testing.T, chunkCount int, nodeCount int) {
 						_, err = lstore.Get(ctx, chunk)
 					}
 					if err != nil {
-						log.Warn(fmt.Sprintf("Chunk %s NOT found for id %s", chunk, id))
+						//log.Warn(fmt.Sprintf("Chunk %s NOT found for id %s", chunk, id))
 						localSuccess = false
 						// Do not get crazy with logging the warn message
 						time.Sleep(500 * time.Millisecond)
