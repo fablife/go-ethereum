24875593d161c22375ced89e4820143acf2f794b
Fabio Barone <holistic.computing@gmail.com>
2018-08-02 14:52:55 -0500

    swarm: even less debug output

--- swarm/network/stream/snapshot_sync_test.go
+++ swarm/network/stream/snapshot_sync_test.go
@@ -424,7 +424,7 @@ func testSyncingViaDirectSubscribe(chunkCount int, nodeCount int) error {
 						_, err = lstore.Get(ctx, chunk)
 					}
 					if err != nil {
-						log.Warn(fmt.Sprintf("Chunk %s NOT found for id %s", chunk, id))
+						//log.Warn(fmt.Sprintf("Chunk %s NOT found for id %s", chunk, id))
 						localSuccess = false
 					} else {
 						log.Debug(fmt.Sprintf("Chunk %s IS FOUND for id %s", chunk, id))
